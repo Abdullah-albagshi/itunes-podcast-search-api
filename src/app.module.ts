959a360245279707e4607e161e03aab0beef09ff
Abdullah albagshi <abdullah.albagshi@gmail.com>
2025-07-11 21:18:57 +0300

    Remove AppController and AppService along with their tests from the application. Update AppModule to reflect the removal of controllers and providers.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ItunesModule } from './modules/itunes/itunes.module';
 
 @Module({
   imports: [PrismaModule, ItunesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
